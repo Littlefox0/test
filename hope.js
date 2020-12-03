956e5d40f8fdb98ede9530c77cab479ba82a1cdf
Littlefox0 <polina.redkoles@gmail.com>
2020-12-04 05:35:28 +0800

    не бей

--- hope.js
+++ hope.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", e => {
+/*document.addEventListener("DOMContentLoaded", e => {
 
 	document.querySelector(".add").addEventListener("click", e => {
 		const input = document.querySelector("input")
@@ -6,5 +6,25 @@ document.addEventListener("DOMContentLoaded", e => {
 		document.querySelector(".bounce").style.background = color;
 		console.log(input.value);
 		console.log(e.currentTarget);
+	})
+})*/
+document.addEventListener("DOMContentLoaded", e => {
+	document.querySelector(".tk").addEventListener("click", e => {
+		let message = document.querySelector(".bot");
+		let login = document.querySelector(".puk").innerText;
+			
+			if (login == 'Привет') {
+			  message = 'Привет';
+			} else if (login == 'Пока') {
+			  message = 'Пока';
+			} else if (login == '') {
+			  message = 'Придумай что по лучше :Р';
+			} else if (login == '%') {
+			  message = 'pan';
+			}
+		document.querySelector(".bot").innerText = message;
+		console.log(message);
+		console.log(e.currentTarget);
+
 	})
 })
